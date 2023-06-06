ffdff5de7e803d96619e657f68df9d73b60ae1d2
Supaishichikin <jordan.toko@outlook.fr>
2023-06-06 10:46:24 +0200

    display books and search term filter
    display books and search term filter

--- components/screens/BookScreen.js
+++ components/screens/BookScreen.js
@@ -1,33 +1,59 @@
-import {Text} from "react-native";
+import {Image, Text, TextInput, View} from "react-native";
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 import {useNavigation} from "@react-navigation/native";
+import {LIVRES, CATEGORIES as CATEGORY_LIST} from "../../models/data";
+import {useState} from "react";
 
 
 export default function BookScreen() {
-
+    const BOOKS = LIVRES
+    const CATEGORIES = [CATEGORY_LIST]
     const navigation = useNavigation();
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const filteredBooks = BOOKS.filter((livre) => {
+        const title = livre.titre.toLowerCase();
+        return title.includes(searchTerm.toLowerCase());
+    });
+
     const onSwipe = (gestureName, gestureState) => {
-        const { SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
+        const {SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections;
         switch (gestureName) {
-          case SWIPE_LEFT:
-            navigation.navigate('Book');
-            break;
-          case SWIPE_RIGHT:
-            navigation.navigate('Home');
-            break;
+            case SWIPE_LEFT:
+                navigation.navigate('Book');
+                break;
+            case SWIPE_RIGHT:
+                navigation.navigate('Home');
+                break;
         }
     };
     const config = {
         velocityThreshold: 0.3,
         directionalOffsetThreshold: 80
     };
-  return (
-    <GestureRecognizer
-          onSwipe={(direction, state) => onSwipe(direction, state)}
-          config={config}
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    return (
+        <GestureRecognizer
+            onSwipe={(direction, state) => onSwipe(direction, state)}
+            config={config}
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
         >
-      <Text>Book!</Text>
-    </GestureRecognizer>
-  );
+            <Text>Books!</Text>
+            <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 10, paddingHorizontal: 10 }}
+                placeholder="Search books..."
+                value={searchTerm}
+                onChangeText={setSearchTerm}
+              />
+            <View>
+                {filteredBooks.map((livre) => { // Remove index
+                    return (
+                        <View key={livre.id} style={{padding: 5}}>
+                            <Image source={{uri: livre.imageUrl}} style={{width: 50, height: 50}}/>
+                            <Text>{livre.titre}</Text>
+                        </View>
+                    );
+                })}
+            </View>
+        </GestureRecognizer>
+    );
 }
